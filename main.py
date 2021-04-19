7fafea26a88177d5947760f0229ed620392fb835
fwara07 <fanuelwara05@gmail.com>
2021-04-19 11:08:45 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from csv import reader
+import unicodedata
+import re
+
+
+def remove_accents(raw_text):
+    raw_text = re.sub("[àáâãäå]", "a", raw_text)
+    raw_text = re.sub("[èéêë]", "e", raw_text)
+    raw_text = re.sub("[ìíîï]", "i", raw_text)
+    raw_text = re.sub("[òóôõö]", "o", raw_text)
+    raw_text = re.sub("[ùúûü]", "u", raw_text)
+    raw_text = re.sub("[ýÿ]", "y", raw_text)
+    raw_text = re.sub("[ß]", "ss", raw_text)
+    raw_text = re.sub("[ñ]", "n", raw_text)
+    raw_text = re.sub("[¿]", "", raw_text)
+    raw_text = re.sub("[?]", "", raw_text)
+    raw_text = re.sub("[¡]", "", raw_text)
+    raw_text = re.sub("[!]", "", raw_text)
+    raw_text = re.sub("[.]", "", raw_text)
+    raw_text = re.sub("[\n]", "", raw_text)
+    return raw_text
+
+
+answers = []
+questions = []
+with open("spanishhelper.csv", "r", encoding="UTF-8") as f:
+    csv_reader = reader(f)
+    for row in csv_reader:
+        row[0] = remove_accents(row[0])
+        row[1] = remove_accents(row[1])
+        answers.append(row[0])
+        questions.append(row[1])
+
+run = True
+while run:
+    for index in range(len(questions)):
+        user_input = input(questions[index] + ": ")
+        if user_input == "stop":
+            run = False
+        else:
+            if user_input.lower() == answers[index].lower():
+                print("You go it!")
+            else:
+                print(f"No, it was {answers[index]}", end="\n\n")
